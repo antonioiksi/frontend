47a79cdaa3e3648e20d9e868e6e06609abb07d5b
antonio <antonioiksi@gmail.com>
2018-02-20 10:09:45 +0300

    change server url

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 #This is a pilot react app as backoffice site
+
+Before starting project change IP for backend server and elasticsearch server
+`/src/services/constants.js`
--- src/services/constants.js
+++ src/services/constants.js
@@ -1,4 +1,7 @@
+export const AUTH_SERVER_URL = 'http://172.16.9.131:8000';
+export const BUSINESS_SERVER_URL = 'http://172.16.9.131:8000';
+export const ELASTICSEARCH_URL = 'http://172.16.9.88:9200';
 
-export const AUTH_SERVER_URL = 'http://localhost:8000';
-export const BUSINESS_SERVER_URL = 'http://localhost:8000';
-export const ELASTICSEARCH_URL = 'http://localhost:9200';
+//export const AUTH_SERVER_URL = 'http://localhost:8000';
+//export const BUSINESS_SERVER_URL = 'http://localhost:8000';
+//export const ELASTICSEARCH_URL = 'http://localhost:9200';
