4e7a0817cb1dc0239537f66d55cc6da9e03e02bf
antonio <antonio@depart9.gvc>
2018-02-27 18:12:08 +0300

    setup working on localhost

--- src/services/constants.js
+++ src/services/constants.js
@@ -1,5 +1,5 @@
-export const AUTH_SERVER_URL = 'http://172.16.9.131:8000';
-export const BUSINESS_SERVER_URL = 'http://172.16.9.131:8000';
+export const AUTH_SERVER_URL = 'http://127.0.0.1:8000';
+export const BUSINESS_SERVER_URL = 'http://127.0.0.1:8000';
 export const ELASTICSEARCH_URL = 'http://172.16.9.88:9200';
 
 //export const AUTH_SERVER_URL = 'http://localhost:8000';
