18d6b02e1f9d18872e6590dffa0470fcbbc00549
antonio <antonio@depart9.gvc>
2018-04-06 20:41:14 +0300

    FIX: added ip 172.16.9.134

--- src/services/constants.js
+++ src/services/constants.js
@@ -1,7 +1,11 @@
-export const AUTH_SERVER_URL = 'http://127.0.0.1:8000';
-export const BUSINESS_SERVER_URL = 'http://127.0.0.1:8000';
-export const ELASTICSEARCH_URL = 'http://172.16.9.88:9200';
+// export const AUTH_SERVER_URL = 'http://127.0.0.1:8000';
+// export const BUSINESS_SERVER_URL = 'http://127.0.0.1:8000';
+// export const ELASTICSEARCH_URL = 'http://172.16.9.88:9200';
 
 //export const AUTH_SERVER_URL = 'http://localhost:8000';
 //export const BUSINESS_SERVER_URL = 'http://localhost:8000';
 //export const ELASTICSEARCH_URL = 'http://localhost:9200';
+
+export const AUTH_SERVER_URL = 'http://172.16.9.134:8000';
+export const BUSINESS_SERVER_URL = 'http://172.16.9.134:8000';
+export const ELASTICSEARCH_URL = 'http://172.16.9.88:9200';
