5ecdb1df344992cf2bb46a2f62f7d513872c8a88
antonio <antonio@depart9.gvc>
2018-04-13 13:11:26 +0300

    FIX: rename title

--- src/services/constants.js
+++ src/services/constants.js
@@ -10,5 +10,5 @@ export const AUTH_SERVER_URL = 'http://172.16.9.134:8000';
 export const BUSINESS_SERVER_URL = 'http://172.16.9.134:8000';
 export const ELASTICSEARCH_URL = 'http://172.16.9.88:9200';
 
-export const APP_TITLE = 'Флоризель 2.0';
+export const APP_TITLE = 'FLORIZEL 2.0';
 
